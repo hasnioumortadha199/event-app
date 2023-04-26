6737ed855b052d7fe4188e286116ada7900e61f1
mortadha <hasnioumortadha@gmail.com>
2023-04-26 16:21:49 +0100

    appbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,10 @@ export default function Navbar() {
   return (
     <div>
       <div className="navbar bg-primary text-primary-content">
-        <p className="text-primary-content">Creative World</p>{" "}
+        <img class="h-14" src="./picto.png" alt="image description" />
+        <p className="text-primary-content uppercase font-bold">
+          Creative World
+        </p>{" "}
         <a href="/" className="btn btn-ghost normal-case text-xl">
           Home{" "}
         </a>
