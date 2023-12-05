9e02b7fdf7a15af23fdf64c16bce990cff65ff24
hasnioumortadha199 <hasnioumotadha@gmail.com>
2023-12-05 09:58:56 +0100

    ff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <div className="navbar bg-primary text-primary-content">
-        <img class="h-14" src="./picto.png" alt="image description" />
+      <div className="navbar bg-orange-700 text-primary-content">
+        <img class="h-14" src="./picto2.png" alt="image description" />
         <p className="text-primary-content uppercase font-bold">
-          Creative World
+          Algerie Poultry forum
         </p>{" "}
         <a href="/" className="btn btn-ghost normal-case text-xl">
           Home{" "}
