dc0d3e4be99592c46257eda7fcdf55eaeb58884e
hasnioumortadha199 <hasnioumortadha@gmail.com>
2025-05-25 22:31:54 +0200

    ff

--- src/components/evalution.js
+++ src/components/evalution.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Evalution() {
   useEffect(() => {
-    window.location.href = "https://fmc21.netlify.app/affiche";
+    window.location.href = "https://morserv.com/";
   }, []);
 
   return null;
