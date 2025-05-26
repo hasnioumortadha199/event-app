f31a7d5058dbc7a08029f962de97c39d09abab24
Your Name <your.email@example.com>
2025-05-26 11:23:39 +0100

    fff

--- src/components/redirectcw.js
+++ src/components/redirectcw.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 export default function CreativeWorldRedirect() {
   useEffect(() => {
-    window.location.href = "https://fmc21.netlify.app/affiche";
+    window.location.href = "https://firebasestorage.googleapis.com/v0/b/cword-1d3c0.appspot.com/o/app-release.apk?alt=media&token=886e634b-20a7-4bea-9048-7a10edbfb532";
   }, []);
 
   return null;
